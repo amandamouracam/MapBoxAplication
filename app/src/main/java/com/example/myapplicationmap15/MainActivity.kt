f9dec596316cb23315aae8e8008dd9a7bb7b0278
amanda <amandamouracam@gmail.com>
2021-12-20 22:51:04 -0300

    Adicionando bottom sheet dialog com botões precisando de ajuste.

--- app/src/main/java/com/example/myapplicationmap15/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationmap15/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.myapplicationmap15
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ScrollView
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.ScrollingView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 //import com.example.myapplicationmap15.R
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
@@ -11,6 +16,8 @@ import com.mapbox.mapboxsdk.maps.MapView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mapView: MapView
+    lateinit var btnShowBottomSheet: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +25,22 @@ class MainActivity : AppCompatActivity() {
         Mapbox.getInstance(applicationContext,getString(R.string.my_acess_token))
 
         mapView = findViewById(R.id.mapView)
+        btnShowBottomSheet = findViewById(R.id.idBtnDismiss);
+
+        btnShowBottomSheet.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+            val btnClose = view.findViewById<Button>(R.id.idBtnDismiss)
+
+            btnClose.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.setCancelable(false)
+            dialog.setContentView(view)
+            dialog.show()
+        }
+
+
 
         //mapView.onCreate(savedInstanceState)
 
